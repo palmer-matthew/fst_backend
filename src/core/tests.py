3df42da421d051fa76484ce2b46d6ffd10fd7577
Matthew <palmer.matthew4567@gmail.com>
2020-07-22 16:28:27 -0500

    Made unit test for scholarship api

--- src/core/tests.py
+++ src/core/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test import Client
 from core.models import TestModel
+from core.models import Scholarship
 # Create your tests here.
 
 
@@ -43,9 +44,64 @@ class  TestModelViewTest(TestCase):
         self.assertEqual(data, expected_response)
 
 
+class ScholarshipViewTest(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+    
+    def test_get(self):
+        tst_scholar_model1 = Scholarship(
+            name ="Jimmy Neutron Welfare Scholarship",
+            description="Available for 1st - 2nd year students for a maximum of 7 years",
+            details="Must be involved in co-curricular activities, must have good hygiene, must be smart and not dunce"
+        )
+        tst_scholar_model1.save()
+        tst_scholar_model2 = Scholarship(
+            name ="Grace Kennedy Scholarship",
+            description="Available for 1st - 3nd year students for a maximum of 1 years",
+            details="Must be involved in co-curricular activities, must have good hygiene, must be smart and not dunce"
+        )
+        tst_scholar_model2.save()
+        tst_scholar_model3 = Scholarship(
+            name ="NCB STEM Scholarship",
+            description="Available for 2nd year students going into 3rd year for a maximum of 0.5 years",
+            details="Must be involved in co-curricular activities, must have good hygiene, must be smart and not dunce"
+        )
+        tst_scholar_model3.save()
 
+        response = self.client.get('/scholarship/')
 
+        self.assertEqual(response.status_code, 200)
+
+        # get json data
+        data = response.json()
+
+        #check number of models returned
+        self.assertEqual(len(data), 3)
+
+        #check if api returns data stored
+        expected_response = [{
+                                'id': tst_scholar_model1.id, 
+                                'name': "Jimmy Neutron Welfare Scholarship",
+                                'description': "Available for 1st - 2nd year students for a maximum of 7 years",
+                                'details': "Must be involved in co-curricular activities, must have good hygiene, must be smart and not dunce"
+
+                            }, 
+                            {
+                              'id': tst_scholar_model2.id,
+                              'name': "Grace Kennedy Scholarship",
+                              'description': "Available for 1st - 3nd year students for a maximum of 1 years",
+                              'details': "Must be involved in co-curricular activities, must have good hygiene, must be smart and not dunce"
+
+                            },
+                            {
+                              'id': tst_scholar_model3.id,
+                              'name': "NCB STEM Scholarship",
+                              'description': "Available for 2nd year students going into 3rd year for a maximum of 0.5 years",
+                              'details': "Must be involved in co-curricular activities, must have good hygiene, must be smart and not dunce"
+ 
+                            }]
+        self.assertEqual(data, expected_response)
 
 
 
