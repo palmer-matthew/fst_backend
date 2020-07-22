9e9a0c7c8a34ba18657040219f8aefdc6a1665bd
Dahalia Howell <dahaliahowell.dh@gmail.com>
2020-07-21 23:28:51 -0500

    created event unit test

--- src/core/tests.py
+++ src/core/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.test import Client
 from core.models import TestModel
+from core.models import Event
 # Create your tests here.
 
 
@@ -42,10 +43,39 @@ class  TestModelViewTest(TestCase):
                             }]
         self.assertEqual(data, expected_response)
 
+class EventViewTest(TestCase):
 
+    def setUp(self):
+        self.client = Client()
 
+    def test_get(self):
 
+        event1 = Event(name = 'FST Pre-Orientation', start_date_time = '2020-08-05T09:00:00Z', end_date_time = '2020-08-05T14:00:00Z', location = 'Faculty of Science and Technology')
+        event1.save()
+        event2 = Event(name = 'Fresher\'s Orientation', start_date_time = '2020-08-28T09:00:00Z', end_date_time = '2020-08-28T14:00:00Z', location = 'UWI, Mona')
+        event2.save()
 
+        response = self.client.get('/events/')
 
+        self.assertEqual(response.status_code, 200)
 
+        data = response.json()
 
+        self.assertEqual(len(data), 2)
+
+        expected_response = [{
+                                'id': event1.id,
+                                'name': 'FST Pre-Orientation',
+                                'start_date_time': '2020-08-05T09:00:00Z',
+                                'end_date_time': '2020-08-05T14:00:00Z',
+                                'location': 'Faculty of Science and Technology'
+                            },
+                            {
+                                'id': event2.id,
+                                'name': 'Fresher\'s Orientation',
+                                'start_date_time': '2020-08-28T09:00:00Z',
+                                'end_date_time': '2020-08-28T14:00:00Z',
+                                'location': 'UWI, Mona' 
+                            }]
+
+        self.assertEqual(data, expected_response)
