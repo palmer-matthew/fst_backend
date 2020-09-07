708851e0d17d5a7b262bc29814bab6e1a6f9f495
Matthew <palmer.matthew4567@gmail.com>
2020-09-07 17:35:20 -0500

    Changed settings.py back to original

--- fst_api/settings.py
+++ fst_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'o9x9m=g(_$!u91yovmc7b+n8kxvja)o2b12t40&x4e@8j30+l6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.11']
+ALLOWED_HOSTS = []
 
 
 # Application definition
